ea308d03c66dbe7d3b188181736027c57fffc027
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-02-19 12:00:38 -0300

    Arrumando algumas coisas

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "react-native";
 import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
+import Privacidade from "./src/screens/Privacidade";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <Sobre />
+      <Privacidade />
     </>
   );
 }
--- src/screens/Sobre.js
+++ src/screens/Sobre.js
@@ -1,5 +1,11 @@
-import { Image, Text, View } from "react-native";
-import { A } from "@expo/html-elements";
+import {
+  Image,
+  Linking,
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import movieDB from "../../assets/images/logo-tmdb.png";
 import React from "react";
 import SafeContainer from "../components/SafeContainer";
@@ -9,29 +15,37 @@ export default function Sobre() {
   return (
     <SafeContainer>
       <View style={estilosInicio.subContainer}>
-        <Text style={estilosInicio.subtitulo}>Sobre o app Dá Hora Filmes</Text>
-        <Text style={estilosInicio.textoSobre}>
-          O <Text style={estilosInicio.nomeApp}>Dá Hora Filmes</Text> é um
-          aplicativo com a finalidade de permitir a busca por informações sobre
-          filmes existentes na base de dados pública disponibilizada pelo site
-          The Movie Database (TMDb).
-        </Text>
-        <View style={estilosInicio.viewMovieDB}>
-          <Image source={movieDB} style={estilosInicio.logoMovieDB} />
-        </View>
-        <Text style={estilosInicio.textoSobre}>
-          Ao localizar um filme, o usuário pode visualizar informações como
-          título, data de lançamento, nota média de avaliação e uma breve
-          descrição sobre o filme e, caso queira, salvar estas informações em
-          uma lista no próprio aplicativo para visualização posterior.
-        </Text>
-        <Text style={estilosInicio.textoSobre}>
-          O aplicativo poderá receber novos recursos conforme o feedback e
-          demanda dos usuários.
-        </Text>
-        <Text style={estilosInicio.textoAutoral}>
-          Dá Hora Filmes &copy; 2024
-        </Text>
+        <ScrollView>
+          <Text style={estilosInicio.subtitulo}>
+            Sobre o app Dá Hora Filmes
+          </Text>
+          <Text style={estilosInicio.textoSobre}>
+            O <Text style={estilosInicio.nomeApp}>Dá Hora Filmes</Text> é um
+            aplicativo com a finalidade de permitir a busca por informações
+            sobre filmes existentes na base de dados pública disponibilizada
+            pelo site The Movie Database (TMDb).
+          </Text>
+          <View style={estilosInicio.viewMovieDB}>
+            <Pressable
+              onPress={() => Linking.openURL("https://www.themoviedb.org/")}
+            >
+              <Image source={movieDB} style={estilosInicio.logoMovieDB} />
+            </Pressable>
+          </View>
+          <Text style={estilosInicio.textoSobre}>
+            Ao localizar um filme, o usuário pode visualizar informações como
+            título, data de lançamento, nota média de avaliação e uma breve
+            descrição sobre o filme e, caso queira, salvar estas informações em
+            uma lista no próprio aplicativo para visualização posterior.
+          </Text>
+          <Text style={estilosInicio.textoSobre}>
+            O aplicativo poderá receber novos recursos conforme o feedback e
+            demanda dos usuários.
+          </Text>
+          <Text style={estilosInicio.textoAutoral}>
+            Dá Hora Filmes &copy; 2024
+          </Text>
+        </ScrollView>
       </View>
     </SafeContainer>
   );
