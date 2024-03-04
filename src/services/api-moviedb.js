69bcb82a108b8e37f7223dc7bc12602f189b6a32
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-03-04 11:17:20 -0300

    Criação do .env

---
+++ src/services/api-moviedb.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+/* Usamos o axios para criar uma referência ao endpoint da API/Service (chamada de baseURL) e configuramos também o uso da API Key. */
+
+const apiKey = process.env.EXPO_PUBLIC_API_KEY;
