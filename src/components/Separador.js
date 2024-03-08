9f241247329a0cc37a01a31356f184fa7f0ee087
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-03-08 11:00:44 -0300

    Create Separador.js

---
+++ src/components/Separador.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { estilosInicio } from "../stylesheet/estilos";
+
+export default function Separador() {
+  return (
+    <View style={estilosInicio.viewSeparador}>
+      <Text>Separador</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
